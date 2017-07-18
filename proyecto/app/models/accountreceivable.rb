755075896141bbda8e1f48bd0fe58b3055861afb
daniell21 <alc_daniel_l@hotmail.com>
2017-07-18 02:19:15 -0400

    calculo del total a pagar sin impuestos

--- proyecto/app/models/accountreceivable.rb
+++ proyecto/app/models/accountreceivable.rb
@@ -1,17 +1,31 @@
 class Accountreceivable < ActiveRecord::Base
   belongs_to :client
-  before_create :calculate
+  before_create :calculateCode
+  before_create :calculateBasicAmount
 
 #validates :username, format: { with: /regex/ }
 
 private
-def calculate
+def calculateCode
 	
-	self.total = Settings.monthlyPayment + retentioniva
-print("YA IMPRIMUUUUU")
-	print(client.country)
+	#self.total = Settings.monthlyPayment + retentioniva
+
 	self.code = client.country + client.state + profitCode
-	print("YA FUE")
+
+end
+def calculateBasicAmount
+	resultado = 0
+	client.discounts.each do |discount|
+		resultado = resultado + discount.percentage
+	end
+	
+	
+	self.total = (Settings.monthlyPayment * ( 1 - (resultado.to_f/100)))
+	
+	#raise (Settings.monthlyPayment * ( 1 - (client.discounts.find(1).percentage.to_f/100))).to_yaml
+
+
+
 end
 
 
