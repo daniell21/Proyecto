6154b8959ff4a0291a6f9871c2a22f0cd042a7e6
daniell21 <alc_daniel_l@hotmail.com>
2017-08-07 22:16:42 -0400

    Se acomodo parcialmente la interfaz de tarifas

--- proyecto/app/models/constant.rb
+++ proyecto/app/models/constant.rb
@@ -1,2 +1,8 @@
 class Constant < ActiveRecord::Base
+	validates :monthlyPayment, presence: true
+	validates :installPayment, presence: true
+	validates :completePayment, presence: true
+	validates :reactivationPayment, presence: true
+	validates :tax, presence: true
+
 end
