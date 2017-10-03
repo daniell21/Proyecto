38ea249e7febebbeca6a7edfee882706ccbb6a6c
daniell21 <alc_daniel_l@hotmail.com>
2017-10-02 23:11:59 -0400

    Se acomodo el redireccionamiento inicio sesion

--- proyecto/app/controllers/principal_controller.rb
+++ proyecto/app/controllers/principal_controller.rb
@@ -1,5 +1,8 @@
 class PrincipalController < ApplicationController
 	before_action :authenticate_user!
   def index
+  	if (current_user.role == "admin")
+      redirect_to clients_path
+    end
   end
 end
